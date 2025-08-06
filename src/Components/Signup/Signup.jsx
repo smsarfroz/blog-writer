96051784e08eb084fc37c16f228681fa9df3c0aa
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-08-06 07:20:35 +0000

    create handleSubmit function

--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -1,7 +1,57 @@
+import styles from './Signup.module.css';
+import { useState } from 'react';
+
+
 const Signup = () => {
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+
+        let data = {};
+        formData.forEach((value, key) => {
+            data[key] = value;
+        });
+
+        console.log(data);
+
+        fetch("http://localhost:3000/signup", {
+            mode: 'cors',
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+        })
+        .then(() => {
+            console.log('user created successfully:');
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        })
+    }
     return (
         <div>
             <h1>signup page</h1>
+
+            <form action="/signup" method="post" onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" name="username" id="username"/>
+                </div>
+
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="text" name="password" id="password"/>
+                </div>
+
+                <button type="submit" className={styles.signup}>Sign Up</button>
+            </form>
         </div>
     );
 };
